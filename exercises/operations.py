7197ae0723b99ceb834ec5033dcd448d5c74d4cf
blonardi <lonardibautista@gmail.com>
2022-10-10 22:25:43 -0300

    Agrego archivo con operaciones matematicas

---
+++ exercises/operations.py
@@ -0,0 +1,30 @@
+#operaciones python
+
+from unittest import result
+
+
+numero1 = 3
+numero2 = 1
+print("Valor de 'numero 1' es: ", numero1)
+print("Valor de 'numero 2' es: ", numero2)
+
+resultado = numero1 + numero2 
+print("Operador = | El valor de variable 'resultado es' es ", resultado)
+
+resultado += numero1
+print("Operador += | El valor de variable 'resultado es' es ", resultado)
+
+resultado *= numero1
+print("Operador *= | El valor de variable 'resultado es' es ", resultado)
+
+resultado /= numero1
+print("Operador /= | El valor de variable 'resultado es' es ", resultado)
+
+resultado %= numero1
+print("Operador %= | El valor de variable 'resultado es' es ", resultado)
+
+resultado **= numero1
+print("Operador **= | El valor de variable 'resultado es' es ", resultado)
+
+resultado //= numero1
+print("Operador //= | El valor de variable 'resultado es' es ", resultado)
